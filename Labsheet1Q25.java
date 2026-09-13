6f497c5f430cb2fddedae6f7286d68fd9bf66093
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:02:23 +0530

    Update and rename 25.logicaloperator to Labsheet1Q25.java

--- Labsheet1Q25.java
+++ Labsheet1Q25.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LogicalOperation {
+public class Labsheet1Q25 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first integer: ");
