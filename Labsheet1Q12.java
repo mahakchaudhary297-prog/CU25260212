9eed6e6e8aa47fc3d286d037b64e63fe22f5c459
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 10:46:49 +0530

    Update and rename 12.leapyearcheck to Labsheet1Q12.java

--- Labsheet1Q12.java
+++ Labsheet1Q12.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LeapYear {
+public class Labsheet1Q12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a year: ");
