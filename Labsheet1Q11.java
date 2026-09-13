62359868368c943ce36651f680db432d3220315c
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 10:45:32 +0530

    Update and rename 11.largest of three.java to Labsheet1Q11.java

--- Labsheet1Q11.java
+++ Labsheet1Q11.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LargestThree {
+public class Labsheet1Q11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter three numbers: ");
