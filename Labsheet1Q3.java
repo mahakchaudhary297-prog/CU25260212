584ce7fb42976ac38c8d8086f4f1a5366854dcc2
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:07:28 +0530

    Update and rename 3.even to Labsheet1Q3.java

--- Labsheet1Q3.java
+++ Labsheet1Q3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EvenOdd {
+public class Labsheet1Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
