3c37e05c22ab70d764f72b30c8aca11c7fba59b0
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:09:24 +0530

    Update and rename 4.relationaloperator to LabsheetqQ4.java

--- LabsheetqQ4.java
+++ LabsheetqQ4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class RelationalDemo {
+public class LabsheetqQ4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first number: ");
