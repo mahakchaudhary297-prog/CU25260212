a74ef70e381fe18c997879d6c24f868e11657e2c
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:06:33 +0530

    Update and rename 29.height.java to Labsheet1Q29.java

--- Labsheet1Q29.java
+++ Labsheet1Q29.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HeightClassification {
+public class Labsheet1Q29 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter height in cm: ");
