92ce2b0e4339943f7d971c6d96b37ff86b230cd3
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:18:09 +0530

    Rename LabsheetQ8.java to Labsheet1Q8.java

--- Labsheet1Q8.java
+++ Labsheet1Q8.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LabsheetQ8 {
+public class Labsheet1Q8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a character: ");
