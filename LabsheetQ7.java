2f9c97e0c500885e493e391a266ee64b0a930a18
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:11:41 +0530

    Update and rename 7.divisibilitycheck to LabsheetQ7.java

--- LabsheetQ7.java
+++ LabsheetQ7.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DivisibleCheck {
+public class LabsheetQ7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
