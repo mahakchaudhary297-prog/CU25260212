75067b44ad007709a6af76b1ce321c173a838569
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:57:30 +0530

    Update and rename HalveNumber.java to  Labsheet2Q6.java

--- Labsheet2Q6.java
+++ Labsheet2Q6.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HalveNumber {
+public class Labsheet2Q6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
