e1b0994e125b2c9da48e8b002e1ab21596de2841
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:19:42 +0530

    Update and rename HeronTriangleArea.java to Labsheet2Q1.java

--- Labsheet2Q1.java
+++ Labsheet2Q1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HeronTriangleArea {
+public class Labsheet2Q1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
