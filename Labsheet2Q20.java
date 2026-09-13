3e2e245a4e5a3ad901cae21377384f27d4992ec1
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:47:25 +0530

    Update and rename PasswordCheck.java to Labsheet2Q20.java

--- Labsheet2Q20.java
+++ Labsheet2Q20.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PasswordCheck {
+public class Labsheet2Q20 {
     public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	String password;
