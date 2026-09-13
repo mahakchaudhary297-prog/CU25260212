4087272f237100cc255001156d19b185fd5cc6b5
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:48:14 +0530

    Update and rename FactorsDoWhile.java to Labsheet2Q21.java

--- Labsheet2Q21.java
+++ Labsheet2Q21.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FactorsDoWhile {
+public class Labsheet2Q21 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
