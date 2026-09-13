e8daa488f71b631c17053a7ec5313b99fcde351d
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:10:59 +0530

    Update and rename 6.ascii to LabsheetQ6.java

--- LabsheetQ6.java
+++ LabsheetQ6.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AsciiValue {
+public class LabsheetQ6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a character: ");
