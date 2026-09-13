1ae8db23473be8b0c4f3a760c3f9db12355ae46d
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 10:54:16 +0530

    Update and rename 19.operatorcheck to Labsheet1Q19.java

--- Labsheet1Q19.java
+++ Labsheet1Q19.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class OperatorCheck {
+public class Labsheet1Q19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first number: ");
