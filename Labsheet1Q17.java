a59a4ef47ab4a13afe39857616abc17e11f75020
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 10:51:57 +0530

    Update and rename 17.palindrome to Labsheet1Q17.java

--- Labsheet1Q17.java
+++ Labsheet1Q17.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Palindrome {
+public class Labsheet1Q17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter an integer: ");
