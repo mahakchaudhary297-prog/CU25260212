e2c2c74be260b54028bce47a12006873b9d50762
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 10:56:33 +0530

    Add Labsheet1Q20.java file

--- Labsheet1Q20.java
+++ Labsheet1Q20.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MarriageEligibility {
+public class Labsheet1Q20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter age: ");
