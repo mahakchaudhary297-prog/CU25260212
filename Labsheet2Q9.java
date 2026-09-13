631eace2be72c32bbb60a79ed245a99a25667092
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:32:33 +0530

    Update and rename LexicographicalCompare.java to Labsheet2Q9.java

--- Labsheet2Q9.java
+++ Labsheet2Q9.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LexicographicalCompare {
+public class Labsheet2Q9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
