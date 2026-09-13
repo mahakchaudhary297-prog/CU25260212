e0176da9d8135a13a87394f8bfde9a2a706ad61b
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:55:56 +0530

    Update and rename RandomDivisibleBreak.txt to Labsheet2Q29.java

--- Labsheet2Q29.java
+++ Labsheet2Q29.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class RandomDivisibleBreak {
+public class Labsheet2Q29 {
     public static void main(String[] args) {
         Random random = new Random();
         int attempts = 0;
