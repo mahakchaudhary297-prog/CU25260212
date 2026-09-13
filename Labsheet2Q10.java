033e95423698e6744fb88c8ed1845c7d8d091410
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:34:13 +0530

    Update and rename LeapYearInRange.java to  Labsheet2Q10.java

--- Labsheet2Q10.java
+++ Labsheet2Q10.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LeapYearInRange {
+public class Labsheet2Q10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
