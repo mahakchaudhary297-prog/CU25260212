11e6534bb6caf5d78c0897f07dc49050ed5a4cdf
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:24:57 +0530

    Update and rename UnaryNegation.java to Labsheet2Q5.java

--- Labsheet2Q5.java
+++ Labsheet2Q5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class UnaryNegation {
+public class Labsheet2Q5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
