4bb1f41982ded64b4e93d30cb9b22b0f8acb7e50
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:12:23 +0530

    Update and rename 8.vowel to LabsheetQ8.java

--- LabsheetQ8.java
+++ LabsheetQ8.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VowelConsonant {
+public class LabsheetQ8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a character: ");
