965c31eac998313efb2511ca47e6228417829956
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 10:57:51 +0530

    Update and rename 21.validtriangle.java to Labsheet1Q21.java

--- Labsheet1Q21.java
+++ Labsheet1Q21.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ValidTriangle {
+public class Labsheet1Q21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter three angles: ");
