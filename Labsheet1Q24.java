19b985eae9a0ddff742a4ff5deac5fe50a62bb73
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:01:16 +0530

    Update and rename 24.perfectnumber.java to Labsheet1Q24.java

--- Labsheet1Q24.java
+++ Labsheet1Q24.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PerfectNumber {
+public class Labsheet1Q24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
