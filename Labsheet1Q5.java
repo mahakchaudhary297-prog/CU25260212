345c6a12a1f8e519448128fc30f4d4668e0a2d2f
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:10:05 +0530

    Update and rename 5.typeconversion to Labsheet1Q5.java

--- Labsheet1Q5.java
+++ Labsheet1Q5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TypeConversion {
+public class Labsheet1Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a floating-point number: ");
