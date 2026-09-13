bc141b7ae2e0d4b2f22b683575d397b976e19d29
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:03:24 +0530

    Rename file to Labsheet1Q26.java

--- Labsheet1Q26.java
+++ Labsheet1Q26.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FirstLastDigit {
+public class Labsheet1Q26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a five-digit number: ");
