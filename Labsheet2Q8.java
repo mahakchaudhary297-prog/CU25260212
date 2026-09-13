7da08b85387d4be6be3a85ed44644f6f4c0e24e1
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:31:38 +0530

    Update and rename ValidTriangleAngles.java to Labsheet2Q8.java

--- Labsheet2Q8.java
+++ Labsheet2Q8.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ValidTriangleAngles {
+public class Labsheet2Q8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
