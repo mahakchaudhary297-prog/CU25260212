0cfa890bbab75aa697f88002955394a5206888e1
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:04:39 +0530

    Update and rename 27.profitorloss.java to Labsheet1Q27.java

--- Labsheet1Q27.java
+++ Labsheet1Q27.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ProfitLoss {
+public class Labsheet1Q27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Cost Price: ");
