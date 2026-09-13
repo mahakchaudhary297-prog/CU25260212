1b8e14a9e26abdbdf325e83f9cf64e11ca8580d2
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:23:52 +0530

    Update and rename VisitorCounter.java to Labsheet2Q4.java

--- Labsheet2Q4.java
+++ Labsheet2Q4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VisitorCounter {
+public class Labsheet2Q4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int visitors = 0;
