7ead0558cb93116d7fb12b5e7508c75a672de505
mahakchaudhary297-prog <mahakchaudhary297@gmail.com>
2026-09-13 11:49:27 +0530

    Update and rename ReverseInteger.java to Labsheet2Q22.java

--- Labsheet2Q22.java
+++ Labsheet2Q22.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ReverseInteger {
+public class Labsheet2Q22 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
